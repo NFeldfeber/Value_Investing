c60304049a4aab73e8c637c51916f783c9e06487
Nicolas Feldfeber <nicofeldfeberdesarrollador@gmail.com>
2019-10-12 12:35:48 -0300

    The index is setted up

--- Value_Investing_App/models.py
+++ Value_Investing_App/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Stock(models.Model):
+    company_name = models.CharField(max_length=300)
+    ticker = models.CharField(max_length=10)
